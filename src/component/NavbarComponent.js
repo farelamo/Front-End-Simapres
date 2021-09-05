f704e8555d1f80f0d07e640f66d936f801932801
farelamo <farizfahmi674@gmail.com>
2021-09-05 13:35:40 +0700

    refactor button & API

--- src/component/NavbarComponent.js
+++ src/component/NavbarComponent.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Nav, Navbar, Container, Form, Button } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom';
 
 import logoVokasi from '../assets/images/logo vokasi.png'
 
@@ -28,9 +29,9 @@ const NavbarComponent = () => {
                             <Nav.Link>Prestasi</Nav.Link>
                         </LinkContainer>
                         <Form className="form-inline">
-                        
+                        <Link to={{ pathname: "http://116.193.190.196:3000" }} target="_blank">
                             <Button size="md" className="ps-4 pe-4">Login</Button>
-                       
+                        </Link>
                         </Form>
                     </Nav>
                 </Navbar.Collapse>
