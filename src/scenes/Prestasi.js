11221fcf7e77d21ef72fe7706cc0fd7ad86f9b11
farelamo <farizfahmi674@gmail.com>
2021-09-05 14:01:41 +0700

    refactor API

--- src/scenes/Prestasi.js
+++ src/scenes/Prestasi.js
@@ -9,7 +9,7 @@ async function API(){
     try {
         const config = {
             method: 'get',
-            url: 'http://116.193.190.196:3000/api/mahasiswa'
+            url: 'http://116.193.190.196:3000/api/mahasiswa/:id'
         };
         const response = await axios(config);
         const data = response.data;
