e5719db7f42e96d19af15c1cba49e22d1cc930b9
farelamo <farizfahmi674@gmail.com>
2021-09-05 13:21:38 +0700

    refactor Link

--- src/component/NavbarComponent.js
+++ src/component/NavbarComponent.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Nav, Navbar, Container, Form, Button } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Link } from 'react-router-dom';
 
 import logoVokasi from '../assets/images/logo vokasi.png'
 
